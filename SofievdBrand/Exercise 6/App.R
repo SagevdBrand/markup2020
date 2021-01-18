76d6085944b1d44e9bcd9a57474195b3fa825227
Sofie vd Brand <64579032+SagevdBrand@users.noreply.github.com>
2021-01-18 10:44:27 +0100

    Adding a theme
    Adding theme and making layout better in general

--- SofievdBrand/Exercise 6/App.R
+++ SofievdBrand/Exercise 6/App.R
@@ -5,34 +5,62 @@ data <- read_csv("data/appdata.csv")
 genres <- read_rds("data/genres.Rds") 
 
 
-ui <- fluidPage(
-  h1("Random movie generator"),
-  ("Tired of endless searching on the Google for a movie to watch?"),
-  ("Select two genres and hit the button below to get a movie-suggestion"),
-  
-  ##Input
-  selectInput(inputId = "choice1",
-              label = "Genre 1",
-              choices = c(paste0(genres))), 
-  
-  selectInput(inputId = "choice2",
-              label = "Genre 2",
-              choices = c(paste0(genres))),
-  
-  ## Button
-  submitButton(text = "Get me a movie!", icon = icon("refresh")),
-  
-  ## Output
-  textOutput(outputId = "movie")
-  
-  )
+ui <-
+  shinyUI(fluidPage(includeCSS("bootstrap.css"),
+                        h1("Random movie generator"),
+                        hr(),
+                        h3("Tired of endless searching `on the Google` for a movie to watch?"),
+                        h3("Select two genres and hit the button below to get a movie-suggestion."),
+                        br(),
+                        br(),
+                        ##Input
+                        selectInput(
+                          inputId = "choice1",
+                          label = "Genre 1",
+                          choices = c(paste0(genres))
+                        ), # close select input
+                        
+                        selectInput(
+                          inputId = "choice2",
+                          label = "Genre 2",
+                          choices = c(paste0(genres))
+                        ), # close select input
+                        
+                        ## Button
+                        br(),
+                        actionButton(
+                          inputId = "actionbutton",
+                          label = "Get me a movie!",
+                          icon = icon("refresh")
+                        ), # close action button
+                      
+                      ## Output
+                      br(),
+                      br(),
+                    br(),
+                      h2(textOutput(
+                        outputId = "movie", inline = T) #close textOutput
+                    ), # close h2
+                    br(),
+                    br(),
+                    tags$footer("Full list of movies downloaded from",
+                                a(href = "this kaggle dataset", "https://www.kaggle.com/rounakbanik/the-movies-dataset"))
+                  ) # close fluidPage
+                ) # close ShinyUI
+
+
+
 
 server <- function(input, output) {
   
   
   output$movie <- renderText({
     
-  find_movie(choices = input)
+    
+    input$actionbutton
+    isolate(find_movie(choices = input))
+    
+    
     })
   
   
